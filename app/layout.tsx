c6b9b6078380d540386d935c2cc8df4d1d2fac61
mugiwaraventures <tramposon12345@gmail.com>
2026-03-30 13:45:58 +0100

    refactor(tracking): set native UTMify and Clarity, remove GTM, fix root page SPA routing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type React from "react"
-import { GoogleTagManager } from "@next/third-parties/google"
 import Script from "next/script"
 import "./globals.css"
 
@@ -11,14 +10,58 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark bg-[#0B1120]">
       <head>
-        <GoogleTagManager gtmId="GTM-PK6HB293" />
+        {/* Microsoft Clarity */}
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "utn9owtst6");
+            `
+          }}
+        />
+
+        {/* UTMify Config & Pixel */}
+        <Script
+          id="utmify-pixel"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+             __html: `
+              window.pixelId = "682dffae44b8147a3883c0f";
+              var a = document.createElement("script");
+              a.setAttribute("async", "");
+              a.setAttribute("defer", "");
+              a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+              document.head.appendChild(a);
+
+              window.tiktokPixelId = "65c3e4a682a84c95c83125a5";
+              var b = document.createElement("script");
+              b.setAttribute("async", "");
+              b.setAttribute("defer", "");
+              b.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel-tiktok.js");
+              document.head.appendChild(b);
+             `
+          }}
+        />
+
+        {/* UTMify Passagem */}
+        <Script
+          id="utmify-passagem"
+          strategy="beforeInteractive"
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-sck="true"
+          data-utmify-prevent-subids="true"
+        />
+
         <Script
           type="text/javascript"
           src="https://app.monetizze.com.br/upsell_incorporado.php"
           strategy="beforeInteractive"
-          async
         />
-
       </head>
       <body className="bg-[#0B1120]">{children}</body>
     </html>
